6cbe7437ab87b9a1c16c89657516e71f61093327
yunjae62 <wodbs7758@naver.com>
2024-11-19 16:20:20 +0900

    first commit

---
+++ src/main/java/ex/evencategory/EvenCategoryApplication.java
@@ -0,0 +1,12 @@
+package ex.evencategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenCategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvenCategoryApplication.class, args);
+    }
+}
---
+++ src/test/java/ex/evencategory/EvenCategoryApplicationTests.java
@@ -0,0 +1,12 @@
+package ex.evencategory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenCategoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
