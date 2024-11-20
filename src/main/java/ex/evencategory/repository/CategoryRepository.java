58f183d654c9b3f15beebc63dd5449a1df5003b5
yunjae62 <wodbs7758@naver.com>
2024-11-20 13:43:23 +0900

    feat: code로 카테고리 조회하는 메서드 추가

--- src/main/java/ex/evencategory/repository/CategoryRepository.java
+++ src/main/java/ex/evencategory/repository/CategoryRepository.java
@@ -1,8 +1,10 @@
 package ex.evencategory.repository;
 
 import ex.evencategory.domain.Category;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    Optional<Category> findByCode(String code);
 }
