1ec26f4b41ac963dc4e6e910ca3bde6bb7e5a7c0
yunjae62 <wodbs7758@naver.com>
2024-11-20 13:42:56 +0900

    feat: Product 엔티티 추가

---
+++ src/main/java/ex/evencategory/domain/Product.java
@@ -0,0 +1,39 @@
+package ex.evencategory.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "tb_product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public static Product create(String name, Category category) {
+        Product product = new Product();
+
+        product.name = name;
+        product.category = category;
+
+        return product;
+    }
+}
