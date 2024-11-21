e5edcc268c2055a75fe4a9e7c6cfab31c825572b
yunjae62 <wodbs7758@naver.com>
2024-11-21 15:08:24 +0900

    feat: 하위 카테고리까지 함께 조회하는 메서드 추가

--- src/main/java/ex/evencategory/repository/CategoryRepository.java
+++ src/main/java/ex/evencategory/repository/CategoryRepository.java
@@ -1,10 +1,18 @@
 package ex.evencategory.repository;
 
 import ex.evencategory.domain.Category;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByCode(String code);
+
+    @Query("SELECT c FROM Category c WHERE c.code LIKE :code%")
+    List<Category> findAllSubByCodeLike(String code);
+
+    @Query("SELECT c FROM Category c WHERE c.code LIKE CONCAT(:code, '>%')")
+    List<Category> findOnlySubByCodeLike(String code);
 }
