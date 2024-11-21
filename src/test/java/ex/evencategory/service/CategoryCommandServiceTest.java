e67f682811060eda88a88dc2f9ab98b2e6fa81a7
yunjae62 <wodbs7758@naver.com>
2024-11-22 01:15:02 +0900

    test: assertj 이용해서 테스트 적용

--- src/test/java/ex/evencategory/service/CategoryCommandServiceTest.java
+++ src/test/java/ex/evencategory/service/CategoryCommandServiceTest.java
@@ -1,15 +1,19 @@
 package ex.evencategory.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ex.evencategory.TestDBConfig;
 import ex.evencategory.domain.Category;
 import ex.evencategory.repository.CategoryRepository;
 import java.util.List;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-class CategoryCommandServiceTest {
+class CategoryCommandServiceTest extends TestDBConfig {
 
     @Autowired
     private CategoryRepository categoryRepository;
@@ -17,58 +21,96 @@ class CategoryCommandServiceTest {
     @Autowired
     private CategoryCommandService categoryCommandService;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     @Transactional
     void createCategory() {
-        Category shoes = categoryCommandService.createCategory("신발", "102");
-        Category converse = categoryCommandService.createCategory("신발>컨버스", "102>100");
-        Category slippers = categoryCommandService.createCategory("신발>슬리퍼", "102>101");
+        // when
+        Category shoes = categoryCommandService.createCategory("신발", "111");
+        Category converse = categoryCommandService.createCategory("신발>컨버스", "111>100");
+        Category slippers = categoryCommandService.createCategory("신발>슬리퍼", "111>101");
 
-        System.out.println(shoes);
-        System.out.println(converse);
-        System.out.println(slippers);
+        // then
+        assertThat(shoes.getName()).isEqualTo("신발");
+        assertThat(shoes.getCode()).isEqualTo("111");
+
+        assertThat(converse.getName()).isEqualTo("신발>컨버스");
+        assertThat(converse.getCode()).isEqualTo("111>100");
+
+        assertThat(slippers.getName()).isEqualTo("신발>슬리퍼");
+        assertThat(slippers.getCode()).isEqualTo("111>101");
     }
 
     @Test
     @Transactional
     void updateCategoryName() {
-        List<Category> categories = categoryCommandService.updateCategoryName("상의", "윗옷");
+        // when
+        List<Category> categories = categoryCommandService.updateCategoryName("하의", "바지");
 
-        for (Category category : categories) {
-            System.out.println(category);
-        }
+        // then
+        assertThat(categories.get(0).getName()).isEqualTo("바지");
+        assertThat(categories.get(0).getCode()).isEqualTo("101");
+
+        assertThat(categories.get(1).getName()).isEqualTo("바지>청바지");
+        assertThat(categories.get(1).getCode()).isEqualTo("101>100");
+
+        assertThat(categories.get(2).getName()).isEqualTo("바지>츄리닝");
+        assertThat(categories.get(2).getCode()).isEqualTo("101>101");
     }
 
     @Test
     @Transactional
     void moveCategoryCode() {
-        Category tempCategory = Category.create("바지", "102");
+        // given
+        Category tempCategory = Category.create("바지", "110");
         categoryRepository.save(tempCategory);
 
-        List<Category> categories = categoryCommandService.moveCategoryCode("101", "102");
+        // when
+        List<Category> categories = categoryCommandService.moveCategoryCode("101", "110");
 
-        for (Category category : categories) {
-            System.out.println(category);
-        }
+        // then
+        assertThat(categories.get(0).getName()).isEqualTo("하의");
+        assertThat(categories.get(0).getCode()).isEqualTo("110");
+
+        assertThat(categories.get(1).getName()).isEqualTo("하의>청바지");
+        assertThat(categories.get(1).getCode()).isEqualTo("110>100");
+
+        assertThat(categories.get(2).getName()).isEqualTo("하의>츄리닝");
+        assertThat(categories.get(2).getCode()).isEqualTo("110>101");
     }
 
     @Test
     @Transactional
     void updateCategoryByCode() {
-        List<Category> categories = categoryCommandService.updateCategoryByCode("101", "102", "바지");
+        // when
+        List<Category> categories = categoryCommandService.updateCategoryByCode("101", "110", "바지");
 
-        for (Category category : categories) {
-            System.out.println(category);
-        }
+        // then
+        assertThat(categories.get(0).getName()).isEqualTo("바지");
+        assertThat(categories.get(0).getCode()).isEqualTo("110");
+
+        assertThat(categories.get(1).getName()).isEqualTo("바지>청바지");
+        assertThat(categories.get(1).getCode()).isEqualTo("110>100");
+
+        assertThat(categories.get(2).getName()).isEqualTo("바지>츄리닝");
+        assertThat(categories.get(2).getCode()).isEqualTo("110>101");
     }
 
     @Test
     @Transactional
     void deleteCategoryByCode() {
-        categoryCommandService.createCategory("신발", "102");
-        categoryCommandService.createCategory("신발>컨버스", "102>100");
-        categoryCommandService.createCategory("신발>슬리퍼", "102>101");
+        // given
+        categoryCommandService.createCategory("신발", "112");
+        categoryCommandService.createCategory("신발>컨버스", "112>100");
+        categoryCommandService.createCategory("신발>슬리퍼", "112>101");
 
-        categoryCommandService.deleteCategoryByCode("102");
+        // when
+        categoryCommandService.deleteCategoryByCode("112");
+        List<Category> categoryList = categoryRepository.findAllSubByCodeLike("112");
+
+        // then
+        assertThat(categoryList).isEmpty();
     }
 }
--- src/test/java/ex/evencategory/service/CategoryQueryServiceTest.java
+++ src/test/java/ex/evencategory/service/CategoryQueryServiceTest.java
@@ -1,5 +1,8 @@
 package ex.evencategory.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ex.evencategory.TestDBConfig;
 import ex.evencategory.domain.Category;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -7,36 +10,71 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CategoryQueryServiceTest {
+class CategoryQueryServiceTest extends TestDBConfig {
 
     @Autowired
     private CategoryQueryService categoryQueryService;
 
     @Test
     void getCategoryById() {
+        // when
         Category category = categoryQueryService.getCategoryById(1L);
-        System.out.println("category = " + category);
+
+        // then
+        assertThat(category).isNotNull();
+        assertThat(category.getName()).isEqualTo("상의");
+        assertThat(category.getCode()).isEqualTo("100");
     }
 
     @Test
     void getCategoryByCode() {
+        // when
         Category category = categoryQueryService.getCategoryByCode("100");
-        System.out.println("category = " + category);
+
+        // then
+        assertThat(category).isNotNull();
+        assertThat(category.getName()).isEqualTo("상의");
+        assertThat(category.getCode()).isEqualTo("100");
     }
 
     @Test
     void getAllSubCategoryListByCode() {
+        // when
         List<Category> categoryList = categoryQueryService.getAllSubCategoryListByCode("100");
-        for (Category category : categoryList) {
-            System.out.println("category = " + category);
-        }
+
+        // then
+        assertThat(categoryList).isNotNull();
+        assertThat(categoryList.size()).isEqualTo(4);
+
+        assertThat(categoryList.get(0).getName()).isEqualTo("상의");
+        assertThat(categoryList.get(0).getCode()).isEqualTo("100");
+
+        assertThat(categoryList.get(1).getName()).isEqualTo("상의>후드티");
+        assertThat(categoryList.get(1).getCode()).isEqualTo("100>100");
+
+        assertThat(categoryList.get(2).getName()).isEqualTo("상의>반팔");
+        assertThat(categoryList.get(2).getCode()).isEqualTo("100>101");
+
+        assertThat(categoryList.get(3).getName()).isEqualTo("상의>맨투맨");
+        assertThat(categoryList.get(3).getCode()).isEqualTo("100>102");
     }
 
     @Test
     void getOnlySubCategoryListByCode() {
+        // when
         List<Category> categoryList = categoryQueryService.getOnlySubCategoryListByCode("100");
-        for (Category category : categoryList) {
-            System.out.println("category = " + category);
-        }
+
+        // then
+        assertThat(categoryList).isNotNull();
+        assertThat(categoryList.size()).isEqualTo(3);
+
+        assertThat(categoryList.get(0).getName()).isEqualTo("상의>후드티");
+        assertThat(categoryList.get(0).getCode()).isEqualTo("100>100");
+
+        assertThat(categoryList.get(1).getName()).isEqualTo("상의>반팔");
+        assertThat(categoryList.get(1).getCode()).isEqualTo("100>101");
+
+        assertThat(categoryList.get(2).getName()).isEqualTo("상의>맨투맨");
+        assertThat(categoryList.get(2).getCode()).isEqualTo("100>102");
     }
 }
