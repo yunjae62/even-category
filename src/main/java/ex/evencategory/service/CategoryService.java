5dceb0d862a814f5df0783955e78664ba083597f
yunjae62 <wodbs7758@naver.com>
2024-11-21 15:10:03 +0900

    feat: 카테고리 조회 메서드 추가

--- src/main/java/ex/evencategory/service/CategoryService.java
+++ src/main/java/ex/evencategory/service/CategoryService.java
@@ -1,9 +1,14 @@
 package ex.evencategory.service;
 
+import ex.evencategory.domain.Category;
 import ex.evencategory.repository.CategoryRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j(topic = "category-service")
 @Service
@@ -11,4 +16,38 @@ import org.springframework.stereotype.Service;
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+
+    /**
+     * ID로 카테고리 조회
+     */
+    @Transactional(readOnly = true)
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+            .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "카테고리를 찾을 수 없습니다."));
+    }
+
+    /**
+     * 코드로 카테고리 단일 조회
+     */
+    @Transactional(readOnly = true)
+    public Category getCategoryByCode(String code) {
+        return categoryRepository.findByCode(code)
+            .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "카테고리를 찾을 수 없습니다."));
+    }
+
+    /**
+     * 코드로 루트 + 하위 카테고리 조회
+     */
+    @Transactional(readOnly = true)
+    public List<Category> getAllSubCategoryListByCode(String code) {
+        return categoryRepository.findAllSubByCodeLike(code);
+    }
+
+    /**
+     * 코드로 하위 카테고리만 조회
+     */
+    @Transactional(readOnly = true)
+    public List<Category> getOnlySubCategoryListByCode(String code) {
+        return categoryRepository.findOnlySubByCodeLike(code);
+    }
 }
