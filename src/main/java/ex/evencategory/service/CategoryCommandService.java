4006684eb9c9b1ee2f005b7cc73a3b6d55809f96
yunjae62 <wodbs7758@naver.com>
2024-11-21 19:22:19 +0900

    feat: 카테고리 커맨드 서비스 추가
    - 카테고리 코드 및 이름 수정

---
+++ src/main/java/ex/evencategory/service/CategoryCommandService.java
@@ -0,0 +1,48 @@
+package ex.evencategory.service;
+
+import ex.evencategory.domain.Category;
+import ex.evencategory.repository.CategoryRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j(topic = "category-command-service")
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CategoryCommandService {
+
+    private final CategoryRepository categoryRepository;
+
+    /**
+     * 카테고리 이름 변경 (하위 카테고리까지 모두 변경)
+     */
+    public List<Category> updateCategoryName(String beforeName, String afterName) {
+        List<Category> categoryList = categoryRepository.findAllSubByNameLike(beforeName);
+        categoryList.forEach(category -> category.updateName(beforeName, afterName));
+
+        return categoryRepository.saveAll(categoryList);
+    }
+
+    /**
+     * 카테고리 코드 변경 (하위 카테고리까지 모두 변경)
+     */
+    public List<Category> moveCategoryCode(String beforeCode, String afterCode) {
+        List<Category> categoryList = categoryRepository.findAllSubByCodeLike(beforeCode);
+        categoryList.forEach(category -> category.updateCode(beforeCode, afterCode));
+
+        return categoryRepository.saveAll(categoryList);
+    }
+
+    /**
+     * 카테고리 코드로 코드와 이름 변경 (하위 카테고리까지 모두 변경)
+     */
+    public List<Category> updateCategoryByCode(String beforeCode, String afterCode, String afterName) {
+        List<Category> categoryList = categoryRepository.findAllSubByCodeLike(beforeCode);
+        categoryList.forEach(category -> category.updateByCode(beforeCode, afterCode, afterName));
+
+        return categoryRepository.saveAll(categoryList);
+    }
+}
