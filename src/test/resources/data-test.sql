56061e5fe5f259d0c087b03f96b789214e9e5f3f
yunjae62 <wodbs7758@naver.com>
2024-11-22 00:13:09 +0900

    test: 테스트용 schema, data 추가

---
+++ src/test/resources/data-test.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_category (name, code)
+VALUES ('상의', '100');
+
+INSERT INTO tb_category (name, code)
+VALUES ('상의>후드티', '100>100');
+
+INSERT INTO tb_category (name, code)
+VALUES ('상의>반팔', '100>101');
+
+INSERT INTO tb_category (name, code)
+VALUES ('하의', '101');
+
+INSERT INTO tb_category (name, code)
+VALUES ('하의>청바지', '101>100');
+
+INSERT INTO tb_category (name, code)
+VALUES ('하의>츄리닝', '101>101');
