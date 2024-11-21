4940f5e8a0e18f38d1d56891955f83d386ff448a
yunjae62 <wodbs7758@naver.com>
2024-11-22 01:14:18 +0900

    test: 테스트용 더미 데이터 추가

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -8,6 +8,9 @@ INSERT INTO tb_category (name, code)
 VALUES ('상의>반팔', '100>101');
 
 INSERT INTO tb_category (name, code)
+VALUES ('상의>맨투맨', '100>102');
+
+INSERT INTO tb_category (name, code)
 VALUES ('하의', '101');
 
 INSERT INTO tb_category (name, code)
@@ -15,3 +18,27 @@ VALUES ('하의>청바지', '101>100');
 
 INSERT INTO tb_category (name, code)
 VALUES ('하의>츄리닝', '101>101');
+
+INSERT INTO tb_category (name, code)
+VALUES ('뷰티', '102');
+
+INSERT INTO tb_category (name, code)
+VALUES ('뷰티>클렌징', '102>100');
+
+INSERT INTO tb_category (name, code)
+VALUES ('뷰티>클렌징>페이셜클렌징', '102>100>100');
+
+INSERT INTO tb_category (name, code)
+VALUES ('뷰티>클렌징>페이셜클렌징>클렌징폼', '102>100>100>100');
+
+INSERT INTO tb_category (name, code)
+VALUES ('뷰티>클렌징>페이셜클렌징>비누', '102>100>100>101');
+
+INSERT INTO tb_category (name, code)
+VALUES ('뷰티>클렌징>메이크업클렌징', '102>100>101');
+
+INSERT INTO tb_category (name, code)
+VALUES ('뷰티>클렌징>메이크업클렌징>클렌징오일', '102>100>101>100');
+
+INSERT INTO tb_category (name, code)
+VALUES ('뷰티>클렌징>메이크업클렌징>클렌징워터', '102>100>101>101');
