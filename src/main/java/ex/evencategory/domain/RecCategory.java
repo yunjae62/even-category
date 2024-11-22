8bb1a72d5090144a3bb6548680b6e571dd33d007
yunjae62 <wodbs7758@naver.com>
2024-11-22 15:38:06 +0900

    feat: 계층형 카테고리 엔티티 추가

---
+++ src/main/java/ex/evencategory/domain/RecCategory.java
@@ -0,0 +1,47 @@
+package ex.evencategory.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Entity
+@ToString
+@Table(name = "tb_rec_category", indexes = @Index(name = "idx_rec_category_parent_id", columnList = "parent_id"))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecCategory {
+
+    public static final Long ROOT_ID = 0L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private Long parentId;
+
+    public static RecCategory create(String name, String code, Long parentId) {
+        RecCategory category = new RecCategory();
+
+        category.name = name;
+        category.code = code;
+        category.parentId = parentId;
+
+        return category;
+    }
+}
