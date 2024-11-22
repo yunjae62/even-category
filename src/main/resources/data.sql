a3980ff1bed85d77036b4a2e4b500f31baababe5
yunjae62 <wodbs7758@naver.com>
2024-11-22 15:39:26 +0900

    feat: 계층형 카테고리 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,3 +15,23 @@ VALUES ('하의>청바지', '101>100');
 
 INSERT INTO tb_category (name, code)
 VALUES ('하의>츄리닝', '101>101');
+
+--
+
+INSERT INTO tb_rec_category (id, name, code, parent_id)
+VALUES (1, '상의', '100', 0);
+
+INSERT INTO tb_rec_category (id, name, code, parent_id)
+VALUES (2, '하의', '101', 0);
+
+INSERT INTO tb_rec_category (id, name, code, parent_id)
+VALUES (3, '후드티', '100', 1);
+
+INSERT INTO tb_rec_category (id, name, code, parent_id)
+VALUES (4, '반팔', '101', 1);
+
+INSERT INTO tb_rec_category (id, name, code, parent_id)
+VALUES (5, '청바지', '100', 2);
+
+INSERT INTO tb_rec_category (id, name, code, parent_id)
+VALUES (6, '츄리닝', '101', 2);
