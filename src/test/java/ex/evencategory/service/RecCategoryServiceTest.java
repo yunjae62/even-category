528e4690c191fb868fed908bbec8eef8d2dd1d8f
yunjae62 <wodbs7758@naver.com>
2024-11-22 16:12:55 +0900

    test: code 칼럼 제거 후 테스트 수정

--- src/test/java/ex/evencategory/service/RecCategoryServiceTest.java
+++ src/test/java/ex/evencategory/service/RecCategoryServiceTest.java
@@ -25,15 +25,8 @@ class RecCategoryServiceTest extends TestDBConfig {
         assertThat(categoryList.size()).isEqualTo(4);
 
         assertThat(categoryList.get(0).getName()).isEqualTo("뷰티");
-        assertThat(categoryList.get(0).getCode()).isEqualTo("102");
-
         assertThat(categoryList.get(1).getName()).isEqualTo("클렌징");
-        assertThat(categoryList.get(1).getCode()).isEqualTo("100");
-
         assertThat(categoryList.get(2).getName()).isEqualTo("페이셜클렌징");
-        assertThat(categoryList.get(2).getCode()).isEqualTo("100");
-
         assertThat(categoryList.get(3).getName()).isEqualTo("클렌징폼");
-        assertThat(categoryList.get(3).getCode()).isEqualTo("100");
     }
 }
